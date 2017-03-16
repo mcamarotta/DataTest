f96421417263533a6431f1eba0c6cc6c7fc85636
michel.camarotta <mcamarotta@gmail.com>
2017-03-16 18:05:44 -0300

    change some configurations and fix a sintax issue

--- front-end/js/config.js
+++ front-end/js/config.js
@@ -5,11 +5,12 @@ function Config() {
 	/// 4 = Prow
 	this.algorithms =
 		[	{ "id": 1, "name": "Each choice", "url": "getEachChoice" },
-			{ "id": 2, "name": "All combinataions", "url": "getAllCombination" },
+			{ "id": 2, "name": "All combinations", "url": "getAllCombination" },
 			{ "id": 3, "name": "Simple pairwise", "url": "getSimplePairWise", "usePairs" : true },
 			{ "id": 4, "name": "Prow", "url": 'NOT IMPLEMENTED' }];
 
-	this.urlBase = 'http://localhost:8080/rest/services/';
+	//this.urlBase = 'http://localhost:8080/rest/services/';
+	this.urlBase = 'http://localhost:8080/testdata-0.0.1-SNAPSHOT/rest/services/';
 
 	this.animationDuration = 100;
 };
